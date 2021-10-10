f78e4b571bc61f4e2ed5542df9f831ad58c9c2e3
leonidamaltsev <daf.dof@yandex.ru>
2021-10-10 22:50:57 +0300

    4.3.6. Math.abs [#373616 #214187]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,15 +3,18 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        int x = x1 - x2;
-        int y = y1 - y2;
-        if ((x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)) {
-            return 0;
+        if ((x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) || (x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7)) {
+            return rsl;
         }
 
-        if (x == Math.abs(y)) {
-            rsl = Math.abs(x);
+        if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+            return Math.abs(x1 - x2);
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        int cells = ChessBoard.way(1, 1, 7, 7);
+        System.out.println("Число ходов слона - " + cells);
     }
 }
