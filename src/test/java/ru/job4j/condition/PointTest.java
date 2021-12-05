faf51fda10daf75f8a5b4a794b9695a1ee8b4792
leonidamaltsev <daf.dof@yandex.ru>
2021-12-05 20:51:02 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -10,44 +10,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when20to20then0() {
         double expected = 0;
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(2, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when02to22then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(0, 2);
+        Point b = new Point(2, 2);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when22to20then2() {
         double expected = 2;
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(2, 2);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
